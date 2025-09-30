17ce8604e2c4887cf3c4fd1f6cdc46760db3e348
Martyna73 <martynaasobiechowska@gmail.com>
2025-09-30 20:38:05 +0200

    hello i am trying my best - multiplying added

--- task.py
+++ task.py
@@ -32,6 +32,7 @@
 #Delete these comments before commit!
 #
 #Good luck.
+#from mnozenie_maciezy import multiplying_matrix
 
 class Matrix():
     def __init__(self, arg):
@@ -40,25 +41,12 @@ class Matrix():
 
     def __add__(self, other_matrix):
         summed_matrix = [[], [], []]
-        if type(other_matrix) == int:
+        if type(other_matrix) == int or type(other_matrix) == float:
             for j in range(len(self.arg)):
                 for i in range(len(self.arg[j])):
                     #print(type(self.arg), type(other_matrix))  # other_matrix.jacie
                     the_sum = self.arg[j][i] + other_matrix
                     summed_matrix[j].append(the_sum)
-
-        # elif type(self.arg) != 'Class':
-        #     print('hell yeah')
-        #     for j in range(len(other_matrix.arg[0])):
-        #         for i in range(len(other_matrix.arg[1])):
-        #             self.arg[j][i] = self.arg
-        #             the_sum = self.arg[j][i] + other_matrix
-        #             summed_matrix[j].append(the_sum)
-            # for j in range(len(self.arg)):
-            #     for i in range(len(self.arg[j])):
-            #         # print(type(self.arg), type(other_matrix))  # other_matrix.jacie
-            #         the_sum = self + other_matrix.arg[j][i]
-            #         summed_matrix[j].append(the_sum)
         else:
             for j in range(len(self.arg)):
                 for i in range(len(self.arg[j])):
@@ -77,45 +65,54 @@ class Matrix():
 
         return summed_matrix
 
+    def __sub__(self, other_matrix):
+        subtracted_matrix= [[], [], []]
 
-
-    #p3 = p1 + p2 + po
-    #p3 = p1.__add__(p2).__add__(po)
-    #p5 = 4 + p1
-    #p5= 4.__add__(p1)
-
-    # def sum_matrix(self, arg2):
-    #     summed_matrix = [[],[],[]]
-    #
-    #     for j in range(len(self.arg)):
-    #         for i in range(len(self.arg[j])):
-    #             the_sum = self.add(self.arg[j][i], arg2[j][i])
-    #             #the_sum = self.arg[j][i] + arg2[j][i]
-    #             summed_matrix[j].append(the_sum)
-    #     return summed_matrix
-    #
-    # def subtract_matrix(self, arg2, lambda):
-    #     return do_sth(lambda ())
-
-
-    def substract_matrix(self, arg1, arg2):
-        subtracted_matrix= arg
-        for j in range(len(arg1.arg)):
-            for i in range(len(arg1.arg[j])):
-                the_substract = self.arg[j][i] - arg2[j][i]
-                subtracted_matrix[j].append(the_substract)
+        if type(other_matrix) == int or type(other_matrix) == float:
+            for j in range(len(self.arg)):
+                for i in range(len(self.arg[j])):
+                    the_substract= self.arg[j][i] - other_matrix
+                    subtracted_matrix[j].append(the_substract)
+        else:
+            for j in range(len(self.arg)):
+                for i in range(len(self.arg[j])):
+                    the_substract = self.arg[j][i] - other_matrix.arg[j][i]
+                    subtracted_matrix[j].append(the_substract)
         return subtracted_matrix
 
+    def __rsub__(self, other_matrix):
+        # do dokoncxzenia
+        subtracted_matrix = [[], [], []]
+        for j in range(len(self.arg)):
+            for i in range(len(self.arg[j])):
+                the_subtract = other_arg - self.arg[j][i]
+                subtracted_matrix[j].append(the_subtract)
 
-    def dot_scalar_matrix(self, row1, row2,):
+        return subtracted_matrix
 
-        print(row1, row2)
-        row1v2= row1[0]*row2[0]
-        row2v2 = row1[1] * row2[1]
-        scalar = row1v2 + row2v2
+###############no generalnie to trzeba zrobic takie puste listy pod operacje zeby nie psulo mi rozmiarow NxN bo na razie mam tylko 3x3
+    def __mul__(self, other_matrix):
+        if type(other_matrix) == int or type(other_matrix) == float:
+            multipled_mat = [[],[],[]]
+            for i in range(len(self.arg)):
+                for j in range(len(self.arg[i])):
+                    the_multipled= self.arg[i][j] * other_matrix
+                    multipled_mat[i].append(the_multipled)
 
-        #print("dot scalar: ",scalar)
-        return scalar
+            return multipled_mat
+
+        else:
+            multipled_mat = []
+            for i in range(len(self.arg)):
+                # print(matrix1[i])
+                row1 = []
+                for j in range(len(self.arg[i])):
+                    val1 = self.arg[i][0] * other_matrix.arg[0][j] + self.arg[i][1] * other_matrix.arg[1][j] + \
+                           self.arg[i][2] * other_matrix.arg[2][j]
+                    row1.append(val1)
+                multipled_mat.append(row1)
+
+            return multipled_mat
 
 
 if __name__ == '__main__':
@@ -126,19 +123,26 @@ if __name__ == '__main__':
     mat_3x3v2 = Matrix([[1, 2,5], [3, 4,5], [7,9,8]])
     mat_3x3v3 = Matrix([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
     matrix3x3v4 = Matrix([[3,3,3], [4,4,4], [1,1,1]])
+    matrix4v4 = Matrix([[1,1,1,1],[2,2,2,2], [3,3,3,3],[4,4,4,4]])
 
     #the_sum = mat_3x3v3.sum_matrix(mat_3x3, mat_3x3)
 
     #matrix3 = (mat_3x3v3.__add__(mat_3x3v2)).__add__(nanan)
     #matrix3 = Matrix.__add__(mat_3x3v3,mat_3x3v2)
     #matrix3 = mat_3x3v3 + mat_3x3v3
-    matrix3 = mat_3x3v3 + 5
-    matrix4 = mat_3x3v2 + matrix3x3v4
-    matrix5 = 1 + mat_3x3v3
+    # matrix3 = mat_3x3v3 + 5
+    # matrix4 = mat_3x3v2 + matrix3x3v4
+    # matrix5 = 1 + mat_3x3v3
     #matrix5 = 5.__add__(mat_3x3v3)
-
-    print(matrix3, '\n', matrix4, '\n', matrix5)
+    #print(matrix3, '\n', matrix4, '\n', matrix5)
     #print(int.__add__(3,4))
+    #mat6 = 5 + mat_3x3v3
+    mat7 = mat_3x3v3 * 4
+    mat6= Matrix([[2,2,2],[4,4,4],[6,6,6]])
+    #mat8 = matrix4v4  * 3
+    #print(mat6.dot_product(matrix3x3v4))
+    print("mnożenie: ",mat7)
+
 
 
 #/home/ms/PycharmProjects/level_2_zadan/task1-Martyna73/task.py
